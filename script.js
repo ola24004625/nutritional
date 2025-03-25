3550d7d68bf22cd7eb1b790316fa3fdaf7860339
ola24004625 <OLA24004625@STUDENTS.SOLIHULL.AC.UK>
2025-03-25 10:11:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function showSlide() {
             slide.classList.add("active");
         }
     });
-    index = (index + 1) % slides.length; // Move to the next slide
+    index = (index + 1) % slides.length; 
 }
 
-setInterval(showSlide, 3000); // Change slide every 3 seconds
+setInterval(showSlide, 3000); 
