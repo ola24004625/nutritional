988f6e3178ee6a12de0fe64e9f6a79aee41e2034
ola24004625 <OLA24004625@STUDENTS.SOLIHULL.AC.UK>
2025-03-25 10:10:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ let index = 0;
 const slides = document.querySelectorAll(".slide");
 
 function showSlide() {
-  slides.forEach((slide) => slide.classList.remove("show")); // Hide all slides
-  slides[index].classList.add("show"); // Show current slide
-  index = (index + 1) % slides.length; // Move to the next slide
+    slides.forEach((slide, i) => {
+        slide.classList.remove("active");
+        if (i === index) {
+            slide.classList.add("active");
+        }
+    });
+    index = (index + 1) % slides.length; // Move to the next slide
 }
 
 setInterval(showSlide, 3000); // Change slide every 3 seconds
-
-// Show the first slide initially
-showSlide();
